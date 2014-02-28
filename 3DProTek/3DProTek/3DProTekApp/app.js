cfba8850c2f9ff49fc0fef6ee533d1084c8e7ed3
TrangHanhTran <trang.tran@enouvo.com>
2014-03-01 00:54:42 +1100

    update page title for each page

--- 3DProTek/3DProTek/3DProTekApp/app.js
+++ 3DProTek/3DProTek/3DProTekApp/app.js
@@ -2,7 +2,7 @@
 
 var app = angular.module("app", ['ngRoute']);
 
-// Routings
+// Define routings rules
 app.config(["$routeProvider", function ($routeProvider) {
     $routeProvider.when("/", {
         controller: "homeController",
@@ -27,28 +27,34 @@ app.config(["$routeProvider", function ($routeProvider) {
     .otherwise({ redirectTo: "/" });
 }]);
 
+// Initializing rootScope data. This run() is called after all modules have been loaded
+app.run(["$rootScope", "$location", function ($rootScope, $location) {
+    $rootScope.isActive = function (viewLocation) {
+        return viewLocation === $location.path();
+    };
+}]);
+
 //homeController
 var homeController = ["$scope", function ($scope) {
-    
-
+    document.title = "Home - 3DProTek";
 }];
 
 //pyoController
 var pyoController = ["$scope", function ($scope) {
-
+    document.title = "P Y O - 3DProTek";
 }];
 
 //contactController
 var contactController = ["$scope", function ($scope) {
-
+    document.title = "Contact - 3DProTek";
 }];
 
 //aboutController
-var aboutController = ["$scope", function ($scope) {
-
+var aboutController = ["$scope", "$location", function ($scope, $location) {
+    document.title = "About - 3DProTek";    
 }];
 
 //ourDesignsController
 var ourDesignsController = ["$scope", function ($scope) {
-
+    document.title = "Our 3D Designs - 3DProTek";
 }];
